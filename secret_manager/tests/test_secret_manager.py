e108d6ffe40d476690429f7c1e88c67b8a78e96e
Winston Weinert <winston@ml1.net>
2014-03-30 18:47:43 -0500

    Import StringIO from io when using Python 3

--- secret_manager/tests/test_secret_manager.py
+++ secret_manager/tests/test_secret_manager.py
@@ -1,6 +1,9 @@
 from .. import secret_manager
 import unittest
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 class Test(unittest.TestCase):
